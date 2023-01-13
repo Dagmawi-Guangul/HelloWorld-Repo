f3401b46bc5ffa16afe4cc1eae59dd19e555b2d8
Dagmawi-Guangul <122639189+Dagmawi-Guangul@users.noreply.github.com>
2023-01-13 13:24:33 -0600

    Add files via upload

---
+++ helloworld.py
@@ -0,0 +1,16 @@
+# 1st Lab question
+print("Hello World")
+
+
+
+
+# 2nd activity
+
+#input the base number
+print("Use this to get the power of any number")
+print("First,your base number")
+#input the power for the base number
+numb1= int(input())
+print("To the power of what?")
+numb2= int(input())
+print("The answer is ",numb1 ** numb2)
